50f70ef5ff74e5eb2ed831cfb8fde29d492892b9
chenyang.fu <1009058470@qq.com>
2020-06-20 17:50:51 +0800

    should_return_a_ticket_given_2_lockers_and_the_first_has_more_capacity_than_2nd_when_smartRobot_save_bag

---
+++ src/main/java/PrimaryRobotLocker/SmartRobot.java
@@ -0,0 +1,9 @@
+package PrimaryRobotLocker;
+
+import java.util.List;
+
+public class SmartRobot extends Robot{
+    public SmartRobot(List<Locker> lockers) {
+        super(lockers);
+    }
+}
--- src/test/java/PrimaryRobotLocker/PrimaryRobotLockerTest.java
+++ src/test/java/PrimaryRobotLocker/PrimaryRobotLockerTest.java
@@ -10,26 +10,6 @@ package PrimaryRobotLocker;
  * Given：Robot 管理两个locker，有一张有效的票；When：robot 取包;  Then: 取包成功。
  */
 
-/*
-given 2个locker,第一个剩余空间大于第二个,when smartLockerRobot存包 then包被存入第一个locker,返回票
-
-given 2个locker, 2个剩余空间相同，when smartLockerRobot存包 then包被存入第一个locker，返回票
-
-given 2个locker, 第2个剩余空间大于第一个，when smartLockerRobot存包 then包被存入第2个locker，返回票
-
-given 2个locker , 没有剩余空间， when smartLockerRobot存包 then存包失败,提示存包失败
-
-given 1个有效票据； when smartLockerRobot取包, then取包成功
-
-given 1个无效票据； when smartLockerRobot取包, then取包失败，提示票据无效
-
-given 1个使用过票据； when smartLockerRobot取包, then取包失败，提示票据已经使用
-
-given primaryLockerRobot和smartLockerRobot同时管理locker，提供1个smartLockerRobot的存包票；when primaryLockerRobot取包；then取包成功
-
-given primaryLockerRobot和smartLockerRobot同时管理locker，提供1个primaryLockerRobot的存包票；when smartLockerRobot取包；then取包成功
- */
-
 import PrimaryRobotLocker.Exception.ExceptionMessages;
 import PrimaryRobotLocker.Exception.PrimaryRobotLockerException;
 import org.junit.Assert;
@@ -131,5 +111,4 @@ public class PrimaryRobotLockerTest {
 
         Assert.assertEquals(bagPickedUp, bag);
     }
-
 }
