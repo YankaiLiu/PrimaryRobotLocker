c5e5fb0b4f9027cbc18488dc0825f730bbbfd1de
Fu Chenyang <61406647+fcy0307@users.noreply.github.com>
2020-07-01 22:58:12 +0800

    should_return_report_when_director_see_report_given_director_managed_1_manager_and_manager_managed_2_lockers_and_1_robot_and_twice_of_locker_have_total_capaction_is_5_and_valid_capaction_is_2_have_valid_capacity_1_and_total_capacity_2_and_robot_have_2_locker_and_them_also_have_5_total_caption_and_2_valid_caption

--- src/test/java/PrimaryLockerRobot/LockerRobotDirectorTest.java
+++ src/test/java/PrimaryLockerRobot/LockerRobotDirectorTest.java
@@ -97,15 +97,7 @@ public class LockerRobotDirectorTest {
         //then
         Assert.assertEquals("M:    2    1\n" + "     L:    2    1",report);
     }
-    /*
-    * Task2:
-- Given: LockerRobotDirector 管理一个 LockerRobotManager, LockerRobotManager 管理两个 Locker ，每个总容量为5，可用容量为2, 不管理 Robot
-- When: LockerRobotDirector 查看报表
-- Then: 返回报表
-    M 4 10
-        L 2 5
-        L 2 5
-        * */
+
     @Test
     public void should_return_report_when_director_see_report_given_director_managed_1_manager_and_manager_managed_2_lockers_and_twice_of_them_have_total_capaction_is_5_and_valid_capaction_is_2have_valid_capacity_1_and_total_capacity_2() throws LockerRobotManagerException {
         //given
@@ -124,6 +116,55 @@ public class LockerRobotDirectorTest {
         //then
         Assert.assertEquals(
                 "M:    10    4\n" + "     L:    5    2\n" + "     L:    5    2",
+                report);
+    }
+
+    /*
+    * Task3:
+- Given: LockerRobotDirector 管理一个 LockerRobotManager, LockerRobotManager 管理两个 Locker ，每个总容量为5，可用容量为2, 管理一个 Robot,
+    Robot 管理两个 Locker，一个总容量为5，可用容量为3，一个总容量为8，可用容量为5
+- When: LockerRobotDirector 查看报表
+- Then: 返回报表
+    M 12 23
+        L 2 5
+        L 2 5
+        R 8 13
+            L 3 5
+            L 5 8
+    * */
+    @Test
+    public void should_return_report_when_director_see_report_given_director_managed_1_manager_and_manager_managed_2_lockers_and_1_robot_and_twice_of_locker_have_total_capaction_is_5_and_valid_capaction_is_2_have_valid_capacity_1_and_total_capacity_2_and_robot_have_2_locker_and_them_also_have_5_total_caption_and_2_valid_caption() throws LockerRobotManagerException {
+        //given
+        Locker locker1 = new Locker(5);
+        Locker locker2 = new Locker(5);
+        for(int i = 0;i<3;i++){
+            locker1.store(new Bag(),0);
+            locker2.store(new Bag(),1);
+        }
+
+        Locker robotLocker1 = new Locker(5);
+        Locker robotLocker2 = new Locker(8);
+        for(int i = 0;i <2;i++){
+            robotLocker1.store(new Bag(),0);
+            robotLocker2.store(new Bag(),1);
+        }
+        robotLocker2.store(new Bag(),1);
+        Robot robot = new Robot(Arrays.asList(robotLocker1,robotLocker2));
+        LockerRobotManager lockerRobotManager = new LockerRobotManager(Arrays.asList(locker1,locker2),Arrays.asList(robot));
+
+
+        LockerRobotDirector lockerRobotDirector = new LockerRobotDirector(Arrays.asList(lockerRobotManager));
+
+        //when
+        String report = lockerRobotDirector.report();
+        //then
+        Assert.assertEquals(
+                "M:    23    12\n" +
+                        "     L:    5    2\n" +
+                        "     L:    5    2\n" +
+                        "     R:    13    8\n" +
+                        "         L:    5    3\n" +
+                        "         L:    8    5",
                 report);
     }
 
